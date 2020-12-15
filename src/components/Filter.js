d4770389b91b1d359e1529e7184eeb6078966331
barrakovsky <barrakovsky@gmail.com>
2020-12-14 18:42:50 -0800

    add-redux-to-filter-v0.1

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -4,7 +4,7 @@ import {filterProducts, sortProducts} from "../actions/productActions"
 
 class Filter extends Component {
     render() {
-        return !this.props.filterProducts? (
+        return !this.props.filteredProducts? (
             <div>Loading...</div> 
             ) : (
             <div className="filter">
